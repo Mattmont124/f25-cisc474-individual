ff40f0325ff3f7729a7b11d54484131b46cb7b1d
Matthew <mmontal@udel.edu>
2025-10-27 15:56:29 -0400

    fixed app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 
-import { LinksModule } from './links/links.module';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
@@ -9,7 +8,7 @@ import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [LinksModule, AuthModule],
+  imports: [ AuthModule],
   controllers: [AppController , CourseController],
   providers: [AppService],
 })
