2d2e751d5c02c101e95fea09e8dfa5716db010e7
Matthew <mmontal@udel.edu>
2025-11-09 22:04:10 -0500

    Trying to fix the import message for prismaclient

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { DatabaseSync } from 'node:sqlite';
-import { PrismaService } from '../prisma.service';
-
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
-  providers: [JwtStrategy, AuthService, PrismaService],
   controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, PrismaService],
   exports: [PassportModule],
 })
 export class AuthModule {}
